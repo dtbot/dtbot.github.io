df1e1f2cedceecd47842bcb06531208812066d8b
The Awesomeness <58598175+antonyalex@users.noreply.github.com>
2022-11-01 13:23:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.querySelector(".button1").onclick=()=>{
   location.href = "https://discord.com/oauth2/authorize?client_id=817958045652615188&permissions=1345842256&scope=bot"
 }
+document.querySelector(".banner__close").addEventListener("click", function () {
+  this.closest(".banner").style.display = "none";
+});
